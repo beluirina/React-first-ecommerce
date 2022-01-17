5accc3bdae1f64b17399564d9ff8625336418b4b
Beluirina <beljessikowski@gmail.com>
2022-01-17 13:54:52 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import './App.css';
 import Navbar from './components/NavBar.jsx';
-<<<<<<< HEAD
-import CartWidget from './components/NavBar.jsx';
-import ItemListContainer from './components/ItemListContainer';
-=======
 import ItemListContainer from './components/ItemListContainer.jsx';
->>>>>>> old-state
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <div>
-<<<<<<< HEAD
-         <Navbar/>
-         <CartWidget/>
-      </div>
-        <div>
-           <ItemListContainer/>
-        </div>
-        
-=======
         <Navbar/>
       </div>
          
         <ItemListContainer/>
->>>>>>> old-state
     </div>
   );
 }
